5824e837dac9a4f97b686b188e2b0cf6f8c704bd
Umut Benzer <umutbenzer@gmail.com>
2017-10-21 23:19:46 +0200

    Swtich id's to random names. Add help and hi abilities to bot.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {controller} = require('./botkit')
 const wit = require('./wit')
 const {activePairs, waitList} = require('./firebase')
+const randomName = require('node-random-name')
 
 // this is triggered when a user clicks the send-to-messenger plugin
 controller.on('facebook_optin', (bot, message) => {
@@ -56,7 +57,16 @@ controller.on('message_received', (bot, message) => {
             }
             return lookingForChat(bot, message)
           }
-          return sendText(bot, message.user, 'Dostum dediğini anlamıyorum.')
+          if (session.intents.asksForHelp) {
+            return handleHelp(bot, session.user.id)
+          }
+          if (session.intents.saysHi) {
+            return handleGreeting(bot, session.user.id)
+          }
+          if (session.intents.saysHowAreYou) {
+            return handleHowAreYou(bot, session.user.id)
+          }
+          return sendText(bot, message.user, "Dostum dediğini anlamıyorum. Yardıma ihtiyacın varsa 'BOT: yardım' yaz.")
         })
         .catch((error) => {
           sendText(bot, message.user, 'Zzzt erenköy!')
@@ -69,7 +79,10 @@ controller.on('message_received', (bot, message) => {
 function determineIntent(text) {
   const response = {
     intents: {
+      asksForHelp: false,
       lookingForChat: false,
+      saysHi: false,
+      saysHowAreYou: false,
       wantToEndTheChat: false
     }
   }
@@ -82,10 +95,88 @@ function determineIntent(text) {
           response.intents.lookingForChat = true
         } else if (intent.value === 'chat_end') {
           response.intents.wantToEndTheChat = true
+        } else if (intent.value === 'greeting') {
+          response.intents.saysHi = true
+        } else if (intent.value === 'how_are_you') {
+          response.intents.saysHowAreYou = true
+        } else if (intent.value === 'help') {
+          response.intents.asksForHelp = true
         }
       }
       return response
     })
+}
+
+function handleHelp(bot, user) {
+  const msg1 = 'Merhaba! Benim adım Ortam Çocuğu. Ben bir botum.'
+  const msg2 = 'Muhabbet edecek birini arayanlar bana gelirler ve takma isimler altında sohbet ederler.'
+  const msg3 = `Mesela senin adın bundan sonra ${generateANameForUser(user)}.`
+  const msg4 = "Bana 'Muhabbet edecek birini arıyorum' diye yazdığın zaman, seni başka birine bağlayacağım."
+  const msg5 = 'Bana yazdığın her şeyi ona ileteceğim. İstediğiniz kadar sohbet edebilirsiniz!'
+  const msg6 = "Başka biriyle yazışırken ban 'BOT: ' diye seslenebilirsin."
+  const msg7 = "Örneğin, sohbetini sonlandırmak istersen 'BOT: Muhabbeti bitir' diyebilirsin."
+  const msg8 = "Yardıma ihtiyacın olduğunda 'BOT: yardım' yazabilirsin."
+  const msg9 = 'Umuyorum ilginç, eğlenceli ve sohbeti koyu insanlar ile karşılaşırsın.'
+  const msg10 = 'Lütfen dikkatli ol! Kişisel bilgilerini, şifeni asla paylaşma! Ne benimle, ne diğerleriyle!'
+  const msg11 = 'Kib bye!'
+
+  return sendText(bot, user, msg1)
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg2))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg3))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg4))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg5))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg6))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg7))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg8))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg9))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg10))
+    .then(typing.bind(null, bot, user))
+    .then(waitABit)
+
+    .then(sendText.bind(null, bot, user, msg11))
+}
+
+function waitABit(n) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, n || 1000)
+  })
+}
+
+function handleGreeting(bot, user) {
+  return sendText(bot, user, "Merhaba! Benim adım Ortam Çocuğu. Ben bir botum. Eğer yardıma ihtiyacın varsa 'yardım' yazman yeterli.")
+}
+
+function handleHowAreYou(bot, user) {
+  return sendText(bot, user, "İyiyim, idare ediyorum bir şekilde. Çok naziksin. :-)")
 }
 
 function seen(bot, to) {
@@ -166,7 +257,7 @@ function sendText(bot, to, messageText) {
 function endChat(bot, session) {
   activePairs.doc(session.pairId)
     .delete()
-    .then(() => sendText(bot, session.chattingWith, `${session.user.id} seninle muhabbet etmeyi bıraktı. :(`))
+    .then(() => sendText(bot, session.chattingWith, `${generateANameForUser(session.user.id)} seninle muhabbet etmeyi bıraktı. :(`))
     .then(() => sendText(bot, session.user.id, 'Artık kimseyle muhabbet etmiyorsun. Yine bana kaldın. :D'))
 }
 
@@ -205,9 +296,16 @@ function lookingForChat(bot, message) {
           [candidate]: true
         }))
         .then(() => Promise.all([
-          sendText(bot, message.user, `Şu an ${candidate} ile muhabbet ediyorsun.`),
+          sendText(bot, message.user, `Şu an ${generateANameForUser(candidate)} ile muhabbet ediyorsun.`),
           sendText(bot, candidate,
-            `Demiştim ben sana birini bulurum diye! Şu an ${message.user} ile muhabbet ediyorsun.`)
+            `Demiştim ben sana birini bulurum diye! Şu an \
+${generateANameForUser(message.user)} ile muhabbet ediyorsun.`)
         ]))
     })
+}
+
+function generateANameForUser(userId) {
+  let randomNo = userId
+  while (randomNo > 1) { randomNo /= 10 }
+  return randomName({random: () => randomNo})
 }
